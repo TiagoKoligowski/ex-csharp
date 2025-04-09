3314921aafd6086d9a2397ff21a971240c8c4778
TiagoKoligowski <140561762+TiagoKoligowski@users.noreply.github.com>
2025-04-08 21:11:32 -0300

    Add files via upload

---
+++ ex2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Insira seu ano de nascimento que vou adivinhar sua idade: ");
+int ano = int.Parse(Console.ReadLine());
+
+int idade = 2024 - ano;
+
+Console.WriteLine($"A sua idade é: {idade} anos.");
